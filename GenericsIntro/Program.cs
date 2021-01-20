e0f02d07c32d30dd3c1620adc5097236357184a3
İskender Boynuz <iskenderboynuz90@gmail.com>
2021-01-20 23:51:04 +0300

    20.01.2021

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("İskender");
+
+            
+        }
+    }
+}
